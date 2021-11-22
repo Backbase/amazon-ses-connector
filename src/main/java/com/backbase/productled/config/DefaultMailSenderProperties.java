77d1d6af7a2e5173f6d8422ae8d136f93e8430b1
amifeanyi <ifeanyi@backbase.com>
2021-11-23 00:48:50 +0100

    clean up

--- src/main/java/com/backbase/productled/config/DefaultMailSenderProperties.java
+++ src/main/java/com/backbase/productled/config/DefaultMailSenderProperties.java
@@ -2,7 +2,6 @@ package com.backbase.productled.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -13,7 +12,6 @@ import javax.validation.constraints.Pattern;
 @ConfigurationProperties(prefix = "backbase.mail")
 @Getter
 @Setter
-@ToString
 @Validated
 public class DefaultMailSenderProperties {
 
