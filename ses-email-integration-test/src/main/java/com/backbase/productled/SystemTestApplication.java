a665fd1fc99193d393e5d1bf02b1dcc120041acf
Kamyar Rafati <kamyar@backbase.com>
2022-10-04 19:49:45 +0200

    HRLD-70-Create System Tests - fix Jenkinsfile

--- ses-email-integration-test/src/main/java/com/backbase/productled/SystemTestApplication.java
+++ ses-email-integration-test/src/main/java/com/backbase/productled/SystemTestApplication.java
@@ -1,19 +1,32 @@
 package com.backbase.productled;
 
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SystemTestApplication {
+
+    private static int exitCode = 0;
+
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return () -> exitCode;
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SystemTestApplication.class);
         try {
             context.getBean(SendEmailTest.class).sendEmailTest();
-            System.exit(0);
+            exitCode = 0;
         } catch (Exception e) {
             e.printStackTrace();
-            System.exit(1);
+            exitCode = 1;
         }
+        System.exit(SpringApplication.exit(context));
+        context.close();
     }
+
 }
