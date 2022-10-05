3621694834a71ce6edc326b29c7b030c92547d30
Kamyar Rafati <kamyar@backbase.com>
2022-10-05 18:32:41 +0200

    HRLD-70-Create System Tests - fix Jenkinsfile

--- ci/docker/system-tests/scripts/run-tests.sh
+++ ci/docker/system-tests/scripts/run-tests.sh
@@ -6,5 +6,5 @@ echo "GATEWAY IS SET UP TO ${GATEWAY_BASEURI}"
 #bash check-health.sh "ping" "${GATEWAY_BASEURI}/api/auth/login"
 #bash check-health.sh "health" "${GATEWAY_BASEURI}/actuator/health/liveness"
 #bash check-health.sh "health" "${GATEWAY_BASEURI}/api/communication/actuator/health"
-sleep 60
+sleep 100
 java -jar app.jar "$@"
