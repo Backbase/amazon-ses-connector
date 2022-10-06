b5d098af182f2c7b0197de60fe87d023a858dc2c
Kamyar Rafati <kamyar@backbase.com>
2022-10-06 13:12:35 +0200

    HRLD-70-Create System Tests - fix Jenkinsfile

--- ci/docker/system-tests/scripts/run-tests.sh
+++ ci/docker/system-tests/scripts/run-tests.sh
@@ -4,5 +4,5 @@ set -e
 #bash check-health.sh "ping" "${GATEWAY_BASEURI}/api/auth/login"
 #bash check-health.sh "health" "${GATEWAY_BASEURI}/actuator/health/liveness"
 #bash check-health.sh "health" "${GATEWAY_BASEURI}/api/communication/actuator/health"
-sleep 100
+
 java -jar app.jar "$@"
