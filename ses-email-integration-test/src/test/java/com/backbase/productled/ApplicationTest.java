4cf77fe26e9ffc352dede72e3bdc18ad5b5d46f4
Kamyar Rafati <kamyar@backbase.com>
2022-10-07 16:32:00 +0200

    HRLD-70- fix: remove duplicate properties

--- ses-email-integration-test/src/test/java/com/backbase/productled/ApplicationTest.java
+++ ses-email-integration-test/src/test/java/com/backbase/productled/ApplicationTest.java
@@ -2,11 +2,20 @@ package com.backbase.productled;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class ApplicationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     public void contextLoads() {
+        assertThat(context).isNotNull();
     }
 }
