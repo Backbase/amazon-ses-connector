ce155f26e921c03652dc6930a59d2c1d4178e6ec
Kamyar Rafati <kamyar@backbase.com>
2022-10-07 14:50:38 +0200

    HRLD-70- fix: add test fix: remove duplicate properties

---
+++ ses-email-integration-test/src/test/java/com/backbase/productled/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.backbase.productled;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
