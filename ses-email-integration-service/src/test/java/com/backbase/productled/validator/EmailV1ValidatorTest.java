f0ea3ba3c9407ce164f8c20aa986986ab7cf420c
Kamyar Rafati <kamyar@backbase.com>
2022-10-11 14:28:43 +0200

    HRLD-65- Update Email Connector to Integrate with Communication Service

--- ses-email-integration-service/src/test/java/com/backbase/productled/validator/EmailV1ValidatorTest.java
+++ ses-email-integration-service/src/test/java/com/backbase/productled/validator/EmailV1ValidatorTest.java
@@ -6,7 +6,6 @@ import com.backbase.productled.testutils.EmailV1Factory;
 import com.backbase.productled.util.ErrorCodes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
