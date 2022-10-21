a45d43c5915f1d065159bade120c19bccbf39524
Kamyar Rafati <kamyar@backbase.com>
2022-10-21 16:38:03 +0200

    HRLD-65 fix: sonar warnings

--- ses-email-integration-service/src/main/java/com/backbase/productled/mapper/EmailV1Mapper.java
+++ ses-email-integration-service/src/main/java/com/backbase/productled/mapper/EmailV1Mapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface EmailV1Mapper {
